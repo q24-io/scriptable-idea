b1854d180fec470b5b794e9ce978c456146a0116
di.sun <dsun@thoughtworks.com>
2020-09-30 12:29:46 +0800

    fix background image

---
+++ idea-with-background-image.js
@@ -0,0 +1,59 @@
+let item = await loadItem()
+let imgURL = "https://picsum.photos/500?blur=3"
+let widget = await createWidget(item, imgURL)
+if (!config.runsInWidget) {
+  await widget.presentLarge()
+}
+Script.setWidget(widget)
+Script.complete()
+
+async function createWidget(item, gif) {
+  let author = item.author
+  let w = new ListWidget()
+
+  if (gif != null) {
+    let imgReq = new Request(gif)
+    let img = await imgReq.loadImage()
+    w.backgroundImage = img
+  }
+
+  let tagText = w.addText(item.tag)
+  tagText.font = Font.boldRoundedSystemFont(12)
+  w.addSpacer(4)
+
+  let titleTxt = w.addText(item.idea)
+  titleTxt.font = Font.mediumSystemFont(14)
+  w.addSpacer(8)
+
+  let authorTxt = w.addText(item.author)
+  authorTxt.font = Font.lightRoundedSystemFont(12)
+  authorTxt.textOpacity = 0.9
+  authorTxt.rightAlignText()
+  w.addSpacer(2)
+
+  let introTxt = w.addText(item.intro)
+  introTxt.font = Font.lightRoundedSystemFont(12)
+  introTxt.textOpacity = 0.9
+  introTxt.rightAlignText()
+  w.addSpacer(8)
+
+  let curatorText = w.addText("内容由 " + item.curator + " 提供")
+  curatorText.font = Font.boldSystemFont(10)
+  curatorText.textOpacity = 0.5
+  curatorText.rightAlignText()
+
+  titleTxt.textColor = Color.white()
+  tagText.textColor = Color.green()
+  authorTxt.textColor = Color.white()
+  introTxt.textColor = Color.white()
+  curatorText.textColor = Color.white()
+  w.url = item.url
+  return w
+}
+
+async function loadItem() {
+  let url = "https://q24.io/api/v1/idea"
+  let req = new Request(url)
+  let json = await req.loadJSON()
+  return json
+}
